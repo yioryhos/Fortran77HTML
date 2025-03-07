c725fe654aa676198534c1fc364c0ec80379c947
yioryhos <gkriemba@sch.gr>
2025-03-07 13:22:58 +0200

    Add files via upload

---
+++ ajax.js
@@ -0,0 +1,11 @@
+function comis(sel){
+var inp = document.getElementById("fortran-"+sel);
+var x = inp.innerHTML; x = x.replace(/\n+/g, ';');
+const xhttp = new XMLHttpRequest();
+xhttp.onload = function() {document.getElementById('div-'+sel).innerHTML = this.responseText;}
+xhttp.open("POST", "main.cgi");
+xhttp.send(x);}
+
+function trigger(){
+var z = document.querySelectorAll('[id^=fortran]').length;
+for (var i=1; i < z+1; i++){comis(i.toString());}}
