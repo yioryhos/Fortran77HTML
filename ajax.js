36de2b3f8e87fab947aacd21cc0193379a63036e
yioryhos <gkriemba@sch.gr>
2025-03-07 23:30:12 +0200

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,6 +1,6 @@
 function comis(sel){
 var inp = document.getElementById("fortran-"+sel);
-var x = inp.innerHTML; x = x.replace(/\n+/g, ';');
+var x = inp.innerHTML; x = x.replace(/\n+/g, ';').replace(/  +/g, ' ').replace(/ ;+/g,';').replace(/; +/g,';');
 const xhttp = new XMLHttpRequest();
 xhttp.onload = function() {document.getElementById('div-'+sel).innerHTML = this.responseText;}
 xhttp.open("POST", "main.cgi");
